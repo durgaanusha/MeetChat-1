75e1fc3b85ea48e77457e27aa85f90f6d7f7c97c
durgaanusha <durgaanusha221b@gmail.com>
2021-02-09 18:41:15 +0530

    hi

--- app/src/main/java/com/example/meetchat/LoginActivity.java
+++ app/src/main/java/com/example/meetchat/LoginActivity.java
@@ -4,23 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//.,mnbvcxzzxcvbnm,./
-
-
-
-
-
-
-
-
-
-
-
+        setContentView(R.layout.activity_login);
     }
 }
